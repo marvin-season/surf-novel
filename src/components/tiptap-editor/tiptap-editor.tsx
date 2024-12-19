b245190daf87ee82e668791378846e57bbbc9519
marvin <sea-son@foxmail.com>
2024-12-19 14:48:05 +0800

    feat: code

--- src/components/tiptap-editor/tiptap-editor.tsx
+++ src/components/tiptap-editor/tiptap-editor.tsx
@@ -8,8 +8,20 @@ import Highlight from "@tiptap/extension-highlight";
 import Command from "./command";
 import Mention from "@tiptap/extension-mention";
 import suggestion from "./suggestion";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import BubbleMenuList from "./BubbleMenuList";
+import { all, createLowlight } from "lowlight";
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+// create a lowlight instance with all languages loaded
+const lowlight = createLowlight(all);
 
+lowlight.register("html", html);
+lowlight.register("css", css);
+lowlight.register("js", js);
+lowlight.register("ts", ts);
 const TipTapEditor = () => {
   const editor = useEditor({
     extensions: [
@@ -24,6 +36,9 @@ const TipTapEditor = () => {
         },
         suggestion,
       }),
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
     ],
     onUpdate: ({ editor }) => {
       console.log(editor.storage.markdown.getMarkdown());
@@ -34,7 +49,12 @@ const TipTapEditor = () => {
           "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
       },
     },
-    content: "",
+    content: `<p>
+          That's a boring paragraph followed by a fenced code block:
+        </p>
+        <p>
+          Press Command/Ctrl + Enter to leave the fenced code block and continue typing in boring paragraphs.
+        </p>`,
   });
 
   if (!editor) {
