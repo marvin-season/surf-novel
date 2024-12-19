495e8428bdcb42e7046196d7e90116cb37326f8a
marvin <sea-son@foxmail.com>
2024-12-19 10:14:02 +0800

    feat: Markdown

--- src/components/tiptap-editor/tiptap-editor.tsx
+++ src/components/tiptap-editor/tiptap-editor.tsx
@@ -3,22 +3,30 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Markdown } from "tiptap-markdown";
+import Typography from "@tiptap/extension-typography";
+import Highlight from "@tiptap/extension-highlight";
 
 const TipTapEditor = () => {
   const editor = useEditor({
-    extensions: [StarterKit, Markdown],
+    extensions: [StarterKit, Typography, Highlight, Markdown],
     onUpdate: ({ editor }) => {
       console.log(editor.getHTML());
     },
     editorProps: {
       attributes: {
-        class: "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
+        class:
+          "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
       },
     },
     content: "## h2",
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <EditorContent
+      editor={editor}
+      className="w-full h-full [&_.ProseMirror]:h-full [&_.ProseMirror]:p-4 [&_.ProseMirror]:overflow-y-auto"
+    />
+  );
 };
 
 export default TipTapEditor;
