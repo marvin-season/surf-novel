33134a7320b6a4fc832d1f19cf80c20a158a816e
marvin <sea-son@foxmail.com>
2024-12-19 11:50:39 +0800

    feat: @mention

--- src/components/tiptap-editor/tiptap-editor.tsx
+++ src/components/tiptap-editor/tiptap-editor.tsx
@@ -6,10 +6,24 @@ import { Markdown } from "tiptap-markdown";
 import Typography from "@tiptap/extension-typography";
 import Highlight from "@tiptap/extension-highlight";
 import Command from "./command";
+import Mention from "@tiptap/extension-mention";
+import suggestion from "./suggestion";
 
 const TipTapEditor = () => {
   const editor = useEditor({
-    extensions: [StarterKit, Typography, Highlight, Markdown],
+    extensions: [
+      StarterKit,
+      Typography,
+      Highlight,
+      Markdown,
+
+      Mention.configure({
+        HTMLAttributes: {
+          class: "mention",
+        },
+        suggestion,
+      }),
+    ],
     onUpdate: ({ editor }) => {
       console.log(editor.storage.markdown.getMarkdown());
     },
@@ -19,7 +33,7 @@ const TipTapEditor = () => {
           "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
       },
     },
-    content: "## h2",
+    content: "",
   });
 
   if (!editor) {
