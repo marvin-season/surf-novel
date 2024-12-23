31e5da25fcad5de304d4f26d43c04ad2f47bff9d
marvin <sea-son@foxmail.com>
2024-12-23 11:51:28 +0800

    feat: 从cookies中获取用户信息

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,10 +1,17 @@
 import { prisma } from "@/lib/prisma";
+import { getStore } from "@/lib/store";
 import { NotesResponse } from "@/types/notes";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  // get user from cookies
+  // const cookies = request.cookies;
+  // console.log("user", JSON.parse(cookies.get("user")?.value || "{}"));
+  const user = JSON.parse((await getStore("user")) || "{}");
+  if (!user) {
+    return new Response("Unauthorized", { status: 401 });
+  }
   try {
-    const email = "2764876579@qq.com";
     // 筛选 id, updatedAt, title字段
     const notes = await prisma.note.findMany({
       select: {
@@ -15,12 +22,12 @@ export async function GET(request: NextRequest) {
       },
       where: {
         author: {
-          email,
+          email: user.email,
         },
       },
       orderBy: {
         updatedAt: "desc",
-      }
+      },
     });
 
     return new Response(JSON.stringify(notes), {
@@ -51,7 +58,7 @@ export async function POST(request: Request) {
       },
     });
 
-    return new Response(JSON.stringify({...note, content}), {
+    return new Response(JSON.stringify({ ...note, content }), {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
