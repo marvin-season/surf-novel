072e0251e4aa9f791ada3e80f966eaec17096869
marvin <sea-son@foxmail.com>
2024-12-24 16:45:59 +0800

    feat: Bubble

--- src/app/(main)/favorites/page.tsx
+++ src/app/(main)/favorites/page.tsx
@@ -1,20 +1,25 @@
-"use client"
+"use client";
 
-import { ContentEditor } from '@/components/novel';
-import { useTranslations } from 'next-intl';
+import { ContentEditor } from "@/components/novel";
+import { useTranslations } from "next-intl";
+import { useEffect, useRef } from "react";
 
 export default function FavoritesPage() {
-  const t = useTranslations('favorites');
+  const t = useTranslations("favorites");
+  const divRef = useRef(null);
 
+  useEffect(() => {
+    console.log(divRef)
+  }, [divRef])
   return (
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">{t('title')}</h2>
+        <h2 className="text-3xl font-bold tracking-tight">{t("title")}</h2>
       </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {/* {t('common.favorites.notesList')} */}
 
-        <ContentEditor />
+        <ContentEditor ref={divRef} />
       </div>
     </div>
   );
--- src/components/novel/content-editor.tsx
+++ src/components/novel/content-editor.tsx
@@ -1,10 +1,50 @@
-import { EditorProvider } from "@tiptap/react";
+import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import { DefaultExtentions } from "./extentions";
+import EditorBubble from "./editor-bubble";
 
-export default function ContentEditor() {
+export default function ContentEditor({}) {
   return (
-    <EditorProvider content={""} extensions={[...DefaultExtentions]}>
-      <div>edit content</div>
-    </EditorProvider>
+    <>
+      <EditorProvider content={""} extensions={[...DefaultExtentions]}>
+        <GenerativeBubbleMenu />
+      </EditorProvider>
+    </>
   );
 }
+
+const GenerativeBubbleMenu = ({ children, open, onOpenChange }: any) => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+  return (
+    <EditorBubble
+      tippyOptions={{
+        placement: open ? "bottom-start" : "top",
+        onHidden: () => {
+          onOpenChange(false);
+          editor.chain().unsetHighlight().run();
+        },
+      }}
+      className="flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl"
+    >
+      hi
+      {/* {open && <AISelector open={open} onOpenChange={onOpenChange} />} */}
+      {/* {!open && (
+          <Fragment>
+            <Button
+              className="gap-1 rounded-none text-purple-500"
+              variant="ghost"
+              onClick={() => onOpenChange(true)}
+              size="sm"
+            >
+              <Magic className="h-5 w-5" />
+              Ask AI
+            </Button>
+            {children}
+          </Fragment>
+        )} */}
+    </EditorBubble>
+  );
+};
