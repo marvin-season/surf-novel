c124b19e2f29b90ccdbc537329c538434564aa8a
marvin <sea-son@foxmail.com>
2024-12-25 18:07:57 +0800

    feat: useCompletion

--- src/app/(main)/favorites/page.tsx
+++ src/app/(main)/favorites/page.tsx
@@ -4,27 +4,28 @@ import { Button } from "@/components/ui/button";
 import { fetchApi } from "@/lib/fetch";
 import { useTranslations } from "next-intl";
 import { useEffect, useRef } from "react";
+import { useCompletion } from 'ai/react';
 
 export default function FavoritesPage() {
   const t = useTranslations("favorites");
-  const divRef = useRef(null);
+  const { complete, isLoading, completion } = useCompletion({
+    api: '/api/generate',
+  });
 
-  useEffect(() => {
-    console.log(divRef)
-  }, [divRef])
   return (
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">{t("title")}</h2>
       </div>
+      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6 text-sm shadow-md rounded-md">
+        {
+          isLoading ? <div>Loading...</div> : <div>{completion}</div>
+        }
+      </div>
       <Button onClick={() => {
-        fetchApi('/generate', {
-          method: 'POST',
-          body: JSON.stringify({
-            prompt: 'hi',
-          }),
-        })
+        complete('Hello, world!');
       }}>Generate</Button>
+      
     </div>
   );
 }
