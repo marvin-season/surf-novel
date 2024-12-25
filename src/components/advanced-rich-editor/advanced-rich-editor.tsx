3db547d07d4a6dad1befb676820a26ce3be6c2fe
marvin <sea-son@foxmail.com>
2024-12-25 15:36:55 +0800

    feat: color extension

--- src/components/advanced-rich-editor/advanced-rich-editor.tsx
+++ src/components/advanced-rich-editor/advanced-rich-editor.tsx
@@ -3,6 +3,10 @@ import { RichEditor } from "../rich-editor";
 import GenerativeBubbleMenu from "./ai-feature/generative-bubble-menu";
 import Operator from "../rich-editor/operator/operator";
 import AiWriter from "./extentions/ai-writer/ai-writer";
+import { Separator } from "@/components/ui/separator";
+import { Color } from "@tiptap/extension-color";
+import { TextStyle } from "@tiptap/extension-text-style";
+import { ColorSelector } from "./selector/color-selector";
 
 export default function AdvancedRichEditor({
   content,
@@ -12,15 +16,19 @@ export default function AdvancedRichEditor({
   className?: string;
 }) {
   const [open, setOpen] = useState(false);
+  const [openColor, setOpenColor] = useState(false);
   return (
     <>
       <RichEditor
         content={content}
         className={className}
-        extensions={[AiWriter.configure({})]}
+        extensions={[AiWriter.configure({}), TextStyle, Color]}
       >
         <Operator />
-        <GenerativeBubbleMenu open={open} onOpenChange={setOpen} />
+        <GenerativeBubbleMenu open={open} onOpenChange={setOpen}>
+          <Separator orientation="vertical" />
+          <ColorSelector open={openColor} onOpenChange={setOpenColor} />
+        </GenerativeBubbleMenu>
       </RichEditor>
     </>
   );
