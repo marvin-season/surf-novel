cd682496d98ecc3dc2647667c3ded459ac5c4afc
marvin <sea-son@foxmail.com>
2024-12-26 15:26:08 +0800

    feat: 提示词

--- src/app/api/generate/prompt/index.ts
+++ src/app/api/generate/prompt/index.ts
@@ -1,96 +1,102 @@
-import { CoreMessage } from "ai";
-import { match } from "ts-pattern";
+import { CoreMessage } from 'ai';
+import { match } from 'ts-pattern';
 
-export const getSystemPrompt = (prompt: string, option: string = 'translate', command: string) => {
-
-    const messages = match(option)
-    .with("continue", () => [
+export const getSystemPrompt = (
+  prompt: string,
+  option: string = 'translate',
+  command: string,
+) => {
+  const messages = match(option)
+    .with('continue', () => [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You are an AI writing assistant that continues existing text based on context from prior text. " +
-          "Give more weight/priority to the later characters than the beginning ones. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
-          "Use Markdown formatting when appropriate.",
+          'You are an AI writing assistant that continues existing text based on context from prior text. ' +
+          'Give more weight/priority to the later characters than the beginning ones. ' +
+          'Limit your response to no more than 200 characters, but make sure to construct complete sentences.' +
+          'Use Markdown formatting when appropriate.',
       },
       {
-        role: "user",
+        role: 'user',
         content: prompt,
       },
     ])
-    .with("improve", () => [
+    .with('improve', () => [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You are an AI writing assistant that improves existing text. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
-          "Use Markdown formatting when appropriate.",
+          'You are an AI writing assistant that improves existing text. ' +
+          'Limit your response to no more than 200 characters, but make sure to construct complete sentences.' +
+          'Use Markdown formatting when appropriate.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `The existing text is: ${prompt}`,
       },
     ])
-    .with("shorter", () => [
+    .with('shorter', () => [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You are an AI writing assistant that shortens existing text. " + "Use Markdown formatting when appropriate.",
+          'You are an AI writing assistant that shortens existing text. ' +
+          'Use Markdown formatting when appropriate.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `The existing text is: ${prompt}`,
       },
     ])
-    .with("longer", () => [
+    .with('longer', () => [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You are an AI writing assistant that lengthens existing text. " +
-          "Use Markdown formatting when appropriate.",
+          'You are an AI writing assistant that lengthens existing text. ' +
+          'Use Markdown formatting when appropriate.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `The existing text is: ${prompt}`,
       },
     ])
-    .with("fix", () => [
+    .with('fix', () => [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You are an AI writing assistant that fixes grammar and spelling errors in existing text. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
-          "Use Markdown formatting when appropriate.",
+          'You are an AI writing assistant that fixes grammar and spelling errors in existing text. ' +
+          'Limit your response to no more than 200 characters, but make sure to construct complete sentences.' +
+          'Use Markdown formatting when appropriate.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `The existing text is: ${prompt}`,
       },
     ])
-    .with("zap", () => [
+    .with('zap', () => [
       {
-        role: "system",
+        role: 'system',
         content:
-          "You area an AI writing assistant that generates text based on a prompt. " +
-          "You take an input from the user and a command for manipulating the text" +
-          "Use Markdown formatting when appropriate.",
+          'You area an AI writing assistant that generates text based on a prompt. ' +
+          'You take an input from the user and a command for manipulating the text' +
+          'Use Markdown formatting when appropriate.',
       },
       {
-        role: "user",
+        role: 'user',
         content: `For this text: ${prompt}. You have to respect the command: ${command}`,
       },
     ])
-    .with("translate", () => [
+    .with('translate', () => [
       {
-        role: "system",
-        content: "You are an AI writing assistant that translates text from English to Chinese.",
+        role: 'system',
+        content:
+          '你是一个中英翻译助手！请直接返回翻译结果！！',
       },
+  
       {
-        role: "user",
-        content: `For this text: ${prompt}. You have to translate it to Chinese.`,
+        role: 'user',
+        content: `请翻译: "${prompt}"!`,
       },
     ])
     .run() as CoreMessage[];
 
-    return messages;
-}
+  return messages;
+};
