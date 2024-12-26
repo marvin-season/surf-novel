db183a82733f77661d45cf2d3a1ad155b26abe31
marvin <sea-son@foxmail.com>
2024-12-26 14:31:10 +0800

    feat: save user config

--- src/app/api/user-config/route.ts
+++ src/app/api/user-config/route.ts
@@ -27,3 +27,36 @@ export async function GET(request: NextRequest) {
 
     return NextResponse.json(userConfig);
 }
+
+// 保存 model 配置
+export async function POST(request: NextRequest) {
+    const user = await getLoggedUserInfo();
+    const body = await request.json();
+
+    // exist
+    const existUserConfig = await prisma.userConfig.findUnique({
+        where: {
+            userId: user.id,
+        }
+    })
+
+    if (!existUserConfig) {
+        // create
+        const userConfig = await prisma.userConfig.create({
+            data: {
+                userId: user.id,
+                ...body,
+            }
+        })
+        return NextResponse.json(userConfig);
+    }
+
+    const userConfig = await prisma.userConfig.update({
+        where: {
+            userId: user.id,
+        },
+        data: body,
+    })
+
+    return NextResponse.json(userConfig);
+}
