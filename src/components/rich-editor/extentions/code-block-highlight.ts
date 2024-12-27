12d64a64ea11182ccad30db6db2d76733c2ff901
marvin <sea-son@foxmail.com>
2024-12-27 15:05:51 +0800

    feat: 代码高亮

---
+++ src/components/rich-editor/extentions/code-block-highlight.ts
@@ -0,0 +1,23 @@
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import { all, createLowlight } from 'lowlight'
+
+// create a lowlight instance with all languages loaded
+const lowlight = createLowlight(all)
+import css from 'highlight.js/lib/languages/css'
+import js from 'highlight.js/lib/languages/javascript'
+import ts from 'highlight.js/lib/languages/typescript'
+import html from 'highlight.js/lib/languages/xml'
+lowlight.register('html', html)
+lowlight.register('css', css)
+lowlight.register('js', js)
+lowlight.register('ts', ts)
+lowlight.register('html', html)
+lowlight.register('css', css)
+lowlight.register('js', js)
+lowlight.register('ts', ts)
+
+const CodeBlockHighlight = CodeBlockLowlight.configure({
+  lowlight,
+})
+
+export default CodeBlockHighlight
