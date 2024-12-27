431280767af218471ae98385326ca1aadb727ff0
marvin <sea-son@foxmail.com>
2024-12-27 16:56:15 +0800

    Revert "feat: EditorCommand"
    This reverts commit f0a146d4339a60ceed708b78ef9e73f65383bb48.

--- src/utils/editor.ts
+++ src/utils/editor.ts
@@ -1,20 +1,16 @@
-import type { Editor } from '@tiptap/core'
-import { Fragment, type Node } from '@tiptap/pm/model'
-import { atom } from 'jotai'
-import type { Range } from '@tiptap/core'
+import type { Editor } from "@tiptap/core";
+import { Fragment, type Node } from "@tiptap/pm/model";
+
 // Get the text before a given position in markdown format
 export const getPrevText = (editor: Editor, position: number) => {
-  const nodes: Node[] = []
+  const nodes: Node[] = [];
   editor.state.doc.forEach((node, pos) => {
-    if (pos >= position) return false
-    nodes.push(node)
-    return true
-  })
-  const fragment = Fragment.fromArray(nodes)
-  const doc = editor.state.doc.copy(fragment)
+    if (pos >= position) return false;
+    nodes.push(node);
+    return true;
+  });
+  const fragment = Fragment.fromArray(nodes);
+  const doc = editor.state.doc.copy(fragment);
 
-  return editor.storage.markdown.serializer.serialize(doc) as string
-}
-
-export const queryAtom = atom('')
-export const rangeAtom = atom<Range | null>(null)
+  return editor.storage.markdown.serializer.serialize(doc) as string;
+};
