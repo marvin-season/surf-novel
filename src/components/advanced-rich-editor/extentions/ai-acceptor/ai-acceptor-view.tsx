2a5c63a47d1a552c568da9a1b93d2632a914954a
marvin <sea-son@foxmail.com>
2024-12-28 10:53:29 +0800

    feat: acceptor功能完成

--- src/components/advanced-rich-editor/extentions/ai-acceptor/ai-acceptor-view.tsx
+++ src/components/advanced-rich-editor/extentions/ai-acceptor/ai-acceptor-view.tsx
@@ -1,16 +1,43 @@
 import { Textarea } from '@/components/ui/textarea'
 import { NodeViewProps, NodeViewWrapper } from '@tiptap/react'
+import { useState } from 'react'
+// 这个组件是用来展示ai生成的内容，并且可以被用户编辑，用户可以选择是否接受ai生成的内容 accept or discard,
+// 如果用户选择接受，则将ai生成的内容content替换到当前选择器
+// 如果用户选择丢弃，则将ai生成的内容丢弃，删除当前节点
+const AiAcceptorView = ({ editor, node, deleteNode }: NodeViewProps) => {
+  const [content, setContent] = useState(node.attrs.content)
 
-const AiAcceptorView = ({ node, HTMLAttributes: {
-  'data-content': content
-} }: NodeViewProps) => {
-  console.log({
-    node,
-  })
-  console.log(content)
   return (
     <NodeViewWrapper data-drag-handle>
-      <div className="flex flex-col gap-2 rounded-lg border p-2">{content}</div>
+      <div>
+        {/* 清除默认样式 */}
+        <Textarea
+          className="outline-none resize-none"
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value)
+          }}
+        />
+        <div className="flex gap-2 text-sm cursor-pointer mt-2">
+          <span
+            className="text-blue-400"
+            onClick={() => {
+              deleteNode()
+              editor.chain().focus().insertContentAt(editor.state.selection.from, content).run()
+            }}
+          >
+            Accept
+          </span>
+          <span
+            className="text-red-300"
+            onClick={() => {
+              deleteNode()
+            }}
+          >
+            Discard
+          </span>
+        </div>
+      </div>
     </NodeViewWrapper>
   )
 }
