2fdd2afd185d4ae6300ee6d3aa3a6d1c6c35fc55
marvin <sea-son@foxmail.com>
2024-12-29 17:48:12 +0800

    feat: rich editor editable

--- src/components/advanced-rich-editor/ai-feature/ai-completion-result-panel.tsx
+++ src/components/advanced-rich-editor/ai-feature/ai-completion-result-panel.tsx
@@ -1,6 +1,22 @@
-export default function AiCompleteResultPanel({content}: { content: string }) {
-    return <div>
-        {content}
+import {RichEditorProvider} from "@/components/rich-editor";
+import {useCurrentEditor} from "@tiptap/react";
+import {useEffect} from "react";
 
-    </div>
+export default function AiCompleteResultPanel({content}: { content: string }) {
+
+    return <>
+        <RichEditorProvider editable={() => false} className={'text-[12px] p-4'}>
+            <RichEditor content={content} />
+        </RichEditorProvider>
+
+    </>
+}
+
+const RichEditor = ({content}: { content: string }) => {
+    const {editor} = useCurrentEditor()
+    useEffect(() => {
+        editor?.commands.setContent(content)
+    }, [content, editor]);
+    return <>
+    </>
 }
--- src/components/rich-editor/rich-editor-provider.tsx
+++ src/components/rich-editor/rich-editor-provider.tsx
@@ -3,22 +3,26 @@ import { EditorProvider } from "@tiptap/react";
 import "./styles.css";
 import useExtentions from "./hooks/useExtentions";
 
+
 export default function RichEditorProvider({
   extensions = [],
   className,
   children,
   content,
+                                               editable
 }: {
   extensions?: AnyExtension[];
   className?: string;
   children?: React.ReactNode;
   content?: string;
+  editable?: (this: any, state: any) => boolean
 }) {
   const defaultExtentions = useExtentions();
   return (
     <>
       <EditorProvider
         editorProps={{
+          editable,
           attributes: {
             class: `prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full ${className}`,
           },
