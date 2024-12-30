16f0a1855e45c2fcec67306c7568e15e31d9fbfb
marvin <sea-son@foxmail.com>
2024-12-30 17:47:19 +0800

    feat： 更新种子文件

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,36 +1,72 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-(async () => {
+;(async () => {
   const providers = [
     {
-      name: "ollama",
-      description: "ollama provider",
+      name: 'ollama',
+      description: 'ollama provider',
       dynamic_params: JSON.stringify({
-        base_url: "http://127.0.0.1:11434",
-        model: "qwen2:7b",
+        base_url: {
+          type: 'string',
+          default: 'http://127.0.0.1:11434',
+          placeholder: '请输入base_url',
+        },
+        model: {
+          type: 'select',
+          default: 'qwen2:7b',
+          placeholder: '请输入模型名称',
+          options: [
+            { name: 'qwen2:7b', value: 'qwen2:7b' },
+            { name: 'llama3.1', value: 'llama3.1' },
+          ],
+        },
       }),
     },
     {
-      name: "azure",
-      description: "azure provider",
+      name: 'azure',
+      description: 'azure provider',
       dynamic_params: JSON.stringify({
-        azure_openai_endpoint: "",
-        azure_openai_key: "",
-        open_model_pref: "",
-        embedding_model_pref: "",
+        azure_openai_endpoint: {
+          type: 'string',
+          default: '',
+          placeholder: '请输入azure_openai_endpoint',
+        },
+        azure_openai_key: {
+          type: 'string',
+          default: '',
+          placeholder: '请输入azure_openai_key',
+        },
+        open_model_pref: {
+          type: 'select',
+          default: '',
+          placeholder: '请输入部署的模型名称',
+          options: [
+            { name: 'gpt-4', value: 'gpt-4' },
+            { name: 'gpt-4o', value: 'gpt-4o' },
+          ],
+        },
+        embedding_model_pref: {
+          type: 'select',
+          default: '',
+          placeholder: '请输入embedding模型名称',
+          options: [
+            { name: 'text-embedding-ada-002', value: 'text-embedding-ada-002' },
+            { name: 'text-embedding-3-small', value: 'text-embedding-3-small' },
+          ],
+        },
       }),
     },
-  ];
+  ]
   // init provider config
   try {
     const providerConfig = await prisma.providerInfo.createMany({
       data: providers,
-    });
-    console.log('Seeding completed.');
+    })
+    console.log('Seeding completed.')
   } catch (error) {
-    console.error('Error during seeding:', error);
-    process.exit(1);
+    console.error('Error during seeding:', error)
+    process.exit(1)
   }
-})();
+})()
