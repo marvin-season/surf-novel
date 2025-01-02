ea2352d5b47bffa69fdb16ecf1d1ac54756e75fc
marvin <sea-son@foxmail.com>
2025-01-02 10:46:27 +0800

    feat: alipay

---
+++ src/components/reward-me/alipay.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function ALiPay() {
+  return (
+    <Image
+      src="/reward/alipay.png"
+      width={150}
+      height={150}
+      alt="Picture of the author"
+    />
+  );
+}
