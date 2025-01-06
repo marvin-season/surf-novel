da1d1b60cc813777f735254a943b3fbc3fe2c488
marvin <sea-son@foxmail.com>
2025-01-06 17:24:50 +0800

    op: extract login logic

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { prisma } from "@/lib/prisma";
-import { hash } from "bcryptjs";
-import { compare } from "bcryptjs";
 import { NextResponse } from "next/server";
+import { loginOrRegist } from "@/lib/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -15,35 +13,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // @ts-ignore
       authorize: async (credentials) => {
         console.log("credentials", credentials);
-
-        const { email, password } = credentials as {
-          email: string;
-          password: string;
-        };
-
-        const user = await prisma.user.findUnique({
-          where: { email },
-        });
-
-        if (user) {
-          // 验证密码
-          const isValid = await compare(password, user.password);
-          console.log("user isValid", user, isValid);
-
-          if (isValid) {
-            return { id: user.id, email: user.email };
-          }
-        } else {
-          // 注册新用户
-          const hashedPassword = await hash(password, 12);
-          const newUser = await prisma.user.create({
-            data: {
-              email,
-              password: hashedPassword,
-            },
-          });
-          return { id: newUser.id, email: newUser.email };
-        }
+        return loginOrRegist(
+          credentials as {
+            email: string;
+            password: string;
+          },
+        );
       },
     }),
   ],
--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+import { compare } from "bcryptjs";
 
 export const getLoggedUserInfo = async () => {
   const session = await auth();
@@ -8,4 +11,36 @@ export const getLoggedUserInfo = async () => {
     email: string;
     id: string;
   };
+};
+
+export const loginOrRegist = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (user) {
+    // 验证密码
+    const isValid = await compare(password, user.password);
+    console.log("user isValid", user, isValid);
+
+    if (isValid) {
+      return { id: user.id, email: user.email };
+    }
+  } else {
+    // 注册新用户
+    const hashedPassword = await hash(password, 12);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    return { id: newUser.id, email: newUser.email };
+  }
 };
