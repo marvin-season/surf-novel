082fd1d0430bd7cb0ecd62e97f6214cb1c9bebb1
marvin <sea-son@foxmail.com>
2025-01-06 16:29:05 +0800

    fix: client signout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 // import SignOutServer from "@/components/auth/sign-out-server";
 
-import SignOutClient from "@/components/auth/sign-out-client";
+// import SignOutClient from "@/components/auth/sign-out-client";
 
 export default async function App() {
   return (
-    <>
-      <SignOutClient />
-    </>
+    <div className="">
+      index page
+      {/* <SignOutServer />
+      <SignOutClient /> */}
+    </div>
   );
 }
